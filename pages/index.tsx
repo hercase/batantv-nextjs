9b94e6921e056d0e3e08102a1bc6be2d822b5510
Hernan Case <her.case@live.com.ar>
2020-09-29 11:09:05 -0300

    Add Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ChannelGrid from '../components/ChannelsGrid';
@@ -7,6 +9,10 @@ import Recommended from '../components/Recommended';
 const HomePage: React.FC = () => {
   return (
     <>
+      <Head>
+        <title>BatanTV</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <section className="container mx-auto">
         <ChannelGrid />
