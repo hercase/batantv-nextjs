50371622ef305f1306f2c083dcc0e01d5fdf043a
Cristian Viterales <cviterales@batan.coop>
2020-09-30 08:53:55 -0300

    add: footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,114 +3,95 @@ import SocialLinks from '../SocialLinks';
 const Footer: React.FC = () => {
   return (
     <footer className="text-gray-700 body-font">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-wrap md:textLeft text-center order-first">
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+      <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-no-wrap flex-wrap flex-col">
+        <div className="w-1/3 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <img className="w-24 mb-3" src="/images/logo.png" />
+          </a>
+          <p className="mt-2 text-sm text-gray-500">
+            Batan.TV es la plataforma OTT de video on line y televisión en vivo
+            de la Cooperativa Batan donde podrás disfrutar de un amplio catálogo
+            de contenidos originales de producidos por nuestra ciudad y
+            alrededores.
+          </p>
+        </div>
+        <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+          <div className="lg:w-1/3 md:w-1/2 w-full px-4">
             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
+              PRODUCTOS
             </h2>
             <nav className="list-none mb-10">
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+              <li className="mb-2">
+                <a
+                  className="text-gray-600 hover:text-gray-800"
+                  href="http://www.ecolan.com/"
+                >
+                  Ecolan
+                </a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+              <li className="mb-2">
+                <a
+                  className="text-gray-600 hover:text-gray-800"
+                  href="http://www.caraludme.edu.ar/"
+                >
+                  Caraludme
+                </a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-              </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-800">Ecodev</a>
               </li>
             </nav>
           </div>
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
+          <div className="lg:w-1/3 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-2">
+              LINK UTILES
             </h2>
             <nav className="list-none mb-10">
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-800" href="">
+                  Mi Cuenta
+                </a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-800" href="">
+                  Suscripción
+                </a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-              </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-800" href="">
+                  Ayuda
+                </a>
               </li>
             </nav>
           </div>
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              CATEGORIES
+          <div className="lg:w-1/3 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-2">
+              CONTACTOS
             </h2>
             <nav className="list-none mb-10">
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-800">
+                  Julian Rios 4215 - Batan
+                </a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-800">
+                  info@batan.coop
+                </a>
               </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-              </li>
-              <li>
-                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-800">
+                  + 223 464 3000
+                </a>
               </li>
             </nav>
-          </div>
-          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              SUBSCRIBE
-            </h2>
-            <div className="flex xl:flex-no-wrap md:flex-no-wrap lg:flex-wrap flex-wrap justify-center md:justify-start">
-              <input
-                className="w-40 sm:w-auto bg-gray-100 rounded xl:mr-4 lg:mr-0 sm:mr-4 mr-2 border border-gray-400 focus:outline-none focus:border-indigo-500 text-base py-2 px-4"
-                placeholder="Placeholder"
-                type="text"
-              />
-              <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Button
-              </button>
-            </div>
-            <p className="text-gray-500 text-sm mt-2 md:textLeft text-center">
-              Bitters chicharrones fanny pack
-              <br className="lg:block hidden" />
-              waistcoat green juice
-            </p>
           </div>
         </div>
       </div>
       <div className="bg-gray-200">
-        <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span className="ml-3 text-xl">tailblocks</span>
-          </a>
-          <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
-            © 2020 tailblocks —
-            <a
-              href="https://twitter.com/knyttneve"
-              rel="noopener noreferrer"
-              className="text-gray-600 ml-1"
-              target="_blank"
-            >
-              @knyttneve
-            </a>
+        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p className="text-gray-500 text-sm text-center sm:text-left">
+            © 2020 Copyright - Batan.tv
           </p>
           <SocialLinks />
         </div>
